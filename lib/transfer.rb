3eeb5c88123ede91025b3ef56ad9c36a464dd9fc
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-22 07:48:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if !valid?
+    if !sender.valid?
       "Transaction rejected. Please check your account balance."
     elsif self.status == "complete"
       "Transfer can only happen once."
