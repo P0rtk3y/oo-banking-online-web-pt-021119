3c2f3f2c10d8785bfc6ff52a6ff73325eef4416d
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-22 07:57:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       sender.balance -= amount 
       receiver.deposit(amount) 
       self.status = "complete"
-    elsif sender.balance < amount 
+    elsif !sender.valid? 
      "Transaction rejected. Please check your account balance."
     end 
   end 
